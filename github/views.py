9bb60ee3763c9b61c7e2dde7dc956043a5845577
Ashutosh Das <git.pyprism@gmail.com>
2015-09-14 22:54:50 +0600

    add login view

--- github/views.py
+++ github/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib import auth
+from django.contrib import messages
 # Create your views here.
+
+
+def index(request):
+    return render(request, 'index.html')
+
+
+def login(request):
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = auth.authenticate(username=username, password=password)
+        if user:
+            auth.login(request, user)
+            return redirect('/hiren')
+        else:
+            messages.error(request, 'Username/Password is not valid!')
+            return redirect(request.path)
+    else:
+        return redirect('/')
