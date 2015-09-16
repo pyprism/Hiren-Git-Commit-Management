20dd223b6c5c89d0b0448a7302f898d85dc0bebf
Ashutosh Das <git.pyprism@gmail.com>
2015-09-16 20:20:25 +0600

    add models

--- github/models.py
+++ github/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Hiren(models.Model):
+    access_token = models.CharField(max_length=200)
+    authorized = models.BooleanField(default=False)
+
+
+class Counter(models.Model):
+    number = models.BigIntegerField()
+    date = models.DateTimeField()
