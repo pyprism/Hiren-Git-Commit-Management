6f33c7e8bfacd5e60bc920bb1e1e97eee6db2f63
Ashutosh Das <git.pyprism@gmail.com>
2015-09-18 17:07:24 +0600

    update full function

--- github/tasks.py
+++ github/tasks.py
@@ -2,29 +2,29 @@ __author__ = 'prism'
 import json
 import requests
 from datetime import datetime
+from github.models import Hiren, Counter
 import pytz
-from django.utils import timezone
-# from celery import shared_task
-#
-#
-# @shared_task
-# def nisha():
-#     print('x' + 'hi')
-#     open('test.txt', 'a').close()
+from github.notification import twilio
+from celery import shared_task
 
-
+@shared_task
 def get_data():
+    """
+    Call github api, send sms, update commit activity to db
+    """
     url = 'https://api.github.com/users/pyprism/events?per_page=100'
-    headers = {'Authorization': 'token 9f9b2f31ce0fc1784381202517ce4732536178f8', 'Time-Zone': 'Asia/Dhaka'}
+    nisha = Hiren.objects.get()
+    headers = {'Authorization': 'token ' + nisha.access_token, 'Time-Zone': 'Asia/Dhaka'}
     raw_response = requests.get(url, headers=headers)
     response = raw_response.json()
+    committed_sin = 0  # total commit  :D
     for i in response:
         if 'PushEvent' == i['type']:
             data = i['created_at']
-            print(data[:10] == datetime.now(pytz.timezone('Asia/Dhaka')).strftime("%Y-%m-%d"))
-            #print(data[:10])
-            #print(datetime.now(pytz.timezone('Asia/Dhaka')).strftime("%y-%m-%d"))
-    return response
-    #return datetime.now(pytz.timezone('Asia/Dhaka')).strftime("%d/%m/%y")
+            if data[:10] == datetime.now(pytz.timezone('Asia/Dhaka')).strftime("%Y-%m-%d"):
+                committed_sin = committed_sin + 1
+            if committed_sin:  # if there is no commit
+                twilio()
+    obj = Counter(number=committed_sin)
+    obj.save()
 
-# get_data()
