1f41d1d6163b8f2c221f2002d44751020da9030b
Ashutosh Das <git.pyprism@gmail.com>
2015-09-20 12:50:54 +0600

    remove unneeded feature

--- github/models.py
+++ github/models.py
@@ -7,7 +7,3 @@ class Hiren(models.Model):
     access_token = models.CharField(max_length=200)
     authorized = models.BooleanField(default=False)
 
-
-class Counter(models.Model):
-    number = models.IntegerField
-    date = models.DateField(auto_now_add=True)
